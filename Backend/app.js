fda7a766885514d2542cd3081f2b916987017d21
Naman Gera <43800580+naman1999@users.noreply.github.com>
2022-12-14 11:01:53 -0500

    cleaned up code

--- Backend/app.js
+++ Backend/app.js
@@ -6,72 +6,10 @@ var logger = require('morgan');
 const mountRoutes = require('./routes')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var drainageRouter = require('./routes/drainage')
 var db = require('./queries');
 var app = express();
 mountRoutes(app)
-// const neo4j = require('neo4j-driver')
-// const driver = new neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j", "sherbrooke"));
-// const session = driver.session();
-
-// // // // const cypher = 'MATCH (n) RETURN count(n) as count';
-
-// const upstream = 'MATCH(s:Segment) WHERE s.NodeID = $nodeid OPTIONAL MATCH (s)-[d:upstream*]->(n) WITH s+COLLECT(DISTINCT n) AS v UNWIND v as a RETURN DISTINCT a.NodeID as NodeID';
-
-// const params = { nodeid: '600a1565-ac79-409e-b0e9-bcdc034306ab' };
-// // session.run(cypher2, params)
-// session.run(upstream, params)
-//     .then(result => {
-//         // On result, get count from first record
-//         // const count = result.records[0].get('count');
-//         // Log response
-//         // console.log( count.toNumber() );
-//         return result.records.map(record =>{
-//           console.log(record.get("NodeID"));
-//         });
-//     })
-//     .catch(e => {
-//         // Output the error
-//         console.log(e);
-//     })
-//     .then(() => {
-//         // Close the Session
-//         return session.close();
-//     })
-//     .then(() => {
-//         // Close the Driver
-//         return driver.close();
-//     });
-
-
-// // const downstream = 'MATCH(s:Segment) WHERE s.NodeID = $nodeid OPTIONAL MATCH (s)-[d:downstream*]->(p) WITH s+COLLECT(DISTINCT p) AS v UNWIND v as a RETURN a.NodeID as NodeID'
-// // session.run(downstream, params)
-// //       .then(result => {
-// //         // On result, get count from first record
-// //         // const count = result.records[0].get('NodeID');
-// //         // Log response
-// //         // console.log( count.toNumber() );
-// //         return result.records.map(record =>{
-// //           console.log(record.get("NodeID"));
-// //         });
-// //       })
-// //       .catch(e => {
-// //         // Output the error
-// //         console.log(e);
-// //       })
-// //       .then(() => {
-// //         // Close the Session
-// //         return session.close();
-// //       })
-// //       .then(() => {
-// //         // Close the Driver
-// //         return driver.close();
-// //       });
-
-
-// app.get('/users/2', gdb.getArea)
-// app.get('/users', db.getCatchment)
-// app.get('/users/1', db.getID)
-// app.get('/users/:cordx',db.getID2)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -85,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/drainage', drainageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Backend/routes/drainage.js
@@ -0,0 +1,5 @@
+const Router = require('express-promise-router')
+const router = new Router()
+const db = require('../queries')
+
+module.exports = router;
