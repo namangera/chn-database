7e772130db0df59b4f258cda4c3d24edf33ca67a
Naman Gera <43800580+naman1999@users.noreply.github.com>
2022-12-19 14:21:49 -0500

    turf js dissolve works, new drainage route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../queries');
 const users = require('./users')
+const drainage = require('./drainage')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,7 @@ router.get('/', function(req, res, next) {
 
 module.exports = app => {
   app.use('/users', users)
+  app.use('/drainage', drainage)
   // app.use('/photos', photos)
   // etc..
 }
